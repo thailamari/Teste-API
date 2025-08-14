ad8680aea1cedbba8db9952242f521370ce0a025
Thaila Mariane Agard <thaila.agard@sensedia.local>
2025-08-14 17:18:27 -0300

    adicionado um novo teste com mock

--- test/controller/transferController.test.js
+++ test/controller/transferController.test.js
@@ -6,6 +6,9 @@ const { expect } = require('chai');
 // Aplicação
 const app = require('../../app')
 
+// Mock
+const transferService = require('../../service/transferService');
+
 // Testes
 describe('Transfer Controller', () => {
     describe('POST /transfers', () =>{
@@ -18,6 +21,25 @@ describe('Transfer Controller', () => {
                     amount: 100
                 });
             expect(resposta.status).to.equal(400);    
+        });
+
+         it('Usando Mocks: Quando informo remetente e destinatário inexistente recebo 400', async () =>{
+            // Mocar apenas a função do Service
+            const transferServiceMock = sinon.stub(transferService, 'transfer')
+            transferServiceMock.throws(new Error('Usuário não encontrado'))
+
+            const resposta = await request(app)
+                .post('/transfer')
+                .send({
+                    from: "Thaila",
+                    to: "Wellinton",
+                    amount: 100
+                });
+            expect(resposta.status).to.equal(400);  
+            expect(resposta.body).to.have.property('error', 'Usuário não encontrado')
+            
+            //Reseto o Mock
+            sinon.restore();
         });
     });
 });
